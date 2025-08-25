a8e47af585d62735782707323f8da370fde408df
gesterom <gesterom@gmail.com>
2025-08-25 17:47:25 +0200

    truncate double

--- Common/src/main/java/at/petrak/hexcasting/common/casting/actions/eval/OpForLoop.kt
+++ Common/src/main/java/at/petrak/hexcasting/common/casting/actions/eval/OpForLoop.kt
@@ -10,8 +10,8 @@ import at.petrak.hexcasting.api.casting.getDouble
 object OpForLoop : ConstMediaAction {
     override val argc = 1
     override fun execute(args: List<Iota>, env: CastingEnvironment): List<Iota> {
-        var n = args.getDouble(0, argc)
-        var res = List(n) { DoubleIota(it * 1.0) }
+        val n = args.getDouble(0, argc).toInt()  // Truncate the double to an integer
+        val res = List(n) { i -> DoubleIota(i.toDouble()) }  // Create list of DoubleIota(0.0), ..., DoubleIota(n-1.0)
         return res
     }
 }
